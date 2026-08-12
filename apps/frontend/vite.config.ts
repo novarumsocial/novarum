3775d1a24d387adaf2673415c4c21602ddd1c79b
Izan Gil <66965250+SrIzan10@users.noreply.github.com>
2026-08-12 17:46:34 +0200

    fix: use static adapter

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -1,5 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import packageJson from './package.json';
@@ -30,7 +30,9 @@ export default defineConfig({
           async: true,
         },
       },
-      adapter: adapter(),
+      adapter: adapter({
+              fallback: 'index.html',
+            }),
     }),
   ],
   define: {
